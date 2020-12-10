4cfd2db59ee2d0fb59c9b5828ac55881253b658c
OlhaShestopal <shestopal.olha96@gmail.com>
2020-12-10 22:19:16 +0200

    added modal window

--- js/index.js
+++ js/index.js
@@ -87,4 +87,40 @@ function getZero(num){
     }
     }
     setClock('.timer', deadline);
+
+    //Modal
+
+    const modalTriger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('.modal__close');
+
+    function closeModal(){
+      modal.classList.add('hide');
+      modal.classList.remove('show');
+      document.body.style.overflow = '';
+    }
+
+
+    modalTriger.forEach(e => {
+      e.addEventListener('click', () => {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+      });
+    })
+    
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal){
+        closeModal();
+      }
+    });
+
+    document.addEventListener('keydown', (e) => {
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    });
+
 });
